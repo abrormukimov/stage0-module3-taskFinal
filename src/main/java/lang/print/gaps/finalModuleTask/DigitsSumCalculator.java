b533566bfe6fc606745d621e35421575d88b78e0
abrormukimov <abrormm34@gmail.com>
2022-09-24 22:02:10 +0500

    Finish final tasks

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int result = 0;
+        while (true) {
+            int num = number % 10;
+            result += num;
+            number /= 10;
+            if (number == 0) {
+                break;
+            }
+        }
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,16 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String result = "";
+        while (true) {
+            int num = number % 10;
+            result += num;
+            number /= 10;
+            if (number == 0) {
+                break;
+            }
+        }
+        int finalResult = Integer.parseInt(result);
+        System.out.println(finalResult);
     }
 }
